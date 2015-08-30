e28c5a1e39a0ecc28daeb00fcd05062a36ab2079
Alexander Balagura <davnobezimeni@gmail.com>
2015-08-30 08:56:15 +0300

    ticket:1 Refactor rests

--- apps/thetask/tests.py
+++ apps/thetask/tests.py
@@ -2,11 +2,21 @@ from django.core.urlresolvers import resolve
 from django.contrib.auth.models import User, AnonymousUser
 from django.test import TestCase
 from django.test.client import RequestFactory
+from django.template.loader import render_to_string
 from apps.thetask.views import index
 
 
 # Create your tests here.
 class IndexPageTest(TestCase):
+    def assert_index(self, user):
+        # form request
+        request = self.factory.get('/')
+        request.user = user
+        response = index(request)
+        # get rendered template
+        template = render_to_string('thethask/index.html', user=user)
+        self.assertEqual(template, response.content)
+
     def setUp(self):
         # set up request factory
         self.factory = RequestFactory()
@@ -22,14 +32,8 @@ class IndexPageTest(TestCase):
 
     def test_auth_user(self):
         # For now auth user will see empty body and title - "Hello <user_name> from theTask" 
-        request = self.factory.get('/')
-        request.user = self.user()
-        response = index(request)
-        self.assertInHTML('<title>Hello test from theTask<title>', response.content)
+        self.assert_index(self.user)
 
     def test_anonymous_user(self):
         # For anonymous user there will be button register with id="btn-register"
-        request = self.factory.get('/')
-        request.user = AnonymousUser()
-        response = index(request)
-        self.assertInHTML('<a class="btn btn-primary">Register<a>', response.content)
+        self.assert_index(AnonymousUser())
