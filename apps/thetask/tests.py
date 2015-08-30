6bcf69414502086c6ff8f5f64d59a6573664b81f
Alexander Balagura <davnobezimeni@gmail.com>
2015-08-30 11:36:26 +0300

    ticket:1 Clean tests

--- apps/thetask/tests.py
+++ apps/thetask/tests.py
@@ -1,15 +1,16 @@
 from django.core.urlresolvers import resolve
 from django.contrib.auth.models import User
 from django.test import TestCase
-from django.test.client import RequestFactory, Client
+from django.test.client import Client
 from apps.thetask.views import index
 
 
 # Create your tests here.
 class IndexPageTest(TestCase):
     def setUp(self):
-        # set up request factory
-        self.factory = RequestFactory()
+        """
+            Get client and create user
+        """
         self.client = Client()
         self.user = User.objects.create_user(
             username='test',
@@ -18,17 +19,23 @@ class IndexPageTest(TestCase):
         )
 
     def test_resolve_index(self):
-        # Simple resolve test
+        """
+            Simple resolve test
+        """
         self.assertEqual(resolve('/').func, index)
 
     def test_auth_user(self):
-        # For now auth user request content will content username
+        """
+            For now auth user request content will content username
+        """
         self.client.login(username='test', password='11111')
         response = self.client.get('/')
         self.assertContains(response, self.user.username)
         self.client.logout()
 
     def test_anonymous_user(self):
+        """
+            For anonymous user request content will content Register word
+        """
         response = self.client.get('/')
-        # For anonymous user request content will content Register word
         self.assertContains(response, 'Register')
