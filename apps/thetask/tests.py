a6347baf9de39cc8e1cda683157ae9c3b2bb02b5
Alexander Balagura <davnobezimeni@gmail.com>
2015-08-30 10:33:54 +0300

    ticket:1 Simplified tests. And shame on me..tests pass

--- apps/thetask/tests.py
+++ apps/thetask/tests.py
@@ -1,27 +1,19 @@
 from django.core.urlresolvers import resolve
 from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth import login
 from django.test import TestCase
-from django.test.client import RequestFactory
+from django.test.client import RequestFactory, Client
 from django.template.loader import render_to_string
 from apps.thetask.views import index
 
 
 # Create your tests here.
 class IndexPageTest(TestCase):
-    def assert_index(self, user):
-        # form request
-        request = self.factory.get('/')
-        request.user = user
-        response = index(request)
-        # get rendered template
-        template = render_to_string('thetask/index.html', {'user': user})
-        self.assertEqual(template, response.content)
-        return response
-
     def setUp(self):
         # set up request factory
         self.factory = RequestFactory()
-        self.user = User.objects.create(
+        self.client = Client()
+        self.user = User.objects.create_user(
             username='test',
             email='test@test.com',
             password='11111'
@@ -32,11 +24,13 @@ class IndexPageTest(TestCase):
         self.assertEqual(resolve('/').func, index)
 
     def test_auth_user(self):
-        response = self.assert_index(self.user)
         # For now auth user request content will content username
+        self.client.login(username='test', password='11111')
+        response = self.client.get('/')
         self.assertContains(response, self.user.username)
+        self.client.logout()
 
     def test_anonymous_user(self):
-        response = self.assert_index(AnonymousUser())
+        response = self.client.get('/')
         # For anonymous user request content will content Register word
-        self.assertContains(response, 'register')
+        self.assertContains(response, 'Register')
