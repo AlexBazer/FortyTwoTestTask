17de522f90050eea08284345663f8ec1ffa6e101
Alexander Balagura <davnobezimeni@gmail.com>
2015-10-12 12:59:50 +0300

    ticket:1 add checking filds in response content

--- apps/custom_auth/tests.py
+++ apps/custom_auth/tests.py
@@ -23,3 +23,33 @@ class TestProfile(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
+    def test_fields_existence(self):
+        """
+            Test fields in response content
+        """
+        user = User.objects.get(pk=1)
+        response = self.client.get('/')
+        test_fields = [
+            'first_name',
+            'last_name',
+            'email',
+        ]
+        test_fields_profile = [
+            'birthday',
+            'jubber_id',
+            'skype_id',
+            'boigraphy',
+            'other_contacts',
+        ]
+        for field in test_fields:
+            content = getattr(user, field)
+            if content:
+                self.assertContains(response, content)
+            else:
+                continue
+        for field in test_fields_profile:
+            content = getattr(user.userprofile, field)
+            if content:
+                self.assertContains(response, content)
+            else:
+                continue
