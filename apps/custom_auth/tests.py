ea4d703b5c509c6aae30372adeff47bd89001407
Alexander Balagura <davnobezimeni@gmail.com>
2015-10-12 12:34:22 +0300

    ticket:1 Add index page existance test

--- apps/custom_auth/tests.py
+++ apps/custom_auth/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from django.contrib.auth.models import User
 
@@ -6,9 +6,20 @@ from django.contrib.auth.models import User
 class TestProfile(TestCase):
     fixtures = ['initial_data.json']
 
+    def setUp(self):
+        self.client = Client()
+
     def test_user_additional_data_exists(self):
         """
             Simple test to check fixtures and additional user data
         """
         user = User.objects.get(pk=1)
         self.assertTrue(user.userprofile)
+
+    def test_index_page(self):
+        """
+            Test index page existence
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
