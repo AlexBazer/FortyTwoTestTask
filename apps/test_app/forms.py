8a19bf344e42a1d18b50d7d44d7ac693ab433088
Alexander Balagura <davnobezimeni@gmail.com>
2015-10-16 14:24:24 +0300

    ticket:5 mock at form on edit user page

--- apps/test_app/forms.py
+++ apps/test_app/forms.py
@@ -1,9 +1,23 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput
 
 from test_app.models import CustomUser
 
 
 class CustomUserForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserForm, self).__init__(*args, **kwargs)
+
+        form_control_fields = [
+            'first_name',
+            'last_name',
+            'jabber_id',
+            'skype_id',
+            'biography',
+            'other_contacts',
+        ]
+        for field in form_control_fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = CustomUser
         fields = [
